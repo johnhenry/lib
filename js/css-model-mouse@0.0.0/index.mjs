7514361d642b679b5abf80261ae915abf9d15e3f
(none) <john@iamjohnhenry.com>
2021-10-12 23:51:12 -0700

    css model keyboard

--- js/css-model-mouse@0.0.0/index.mjs
+++ js/css-model-mouse@0.0.0/index.mjs
@@ -1,43 +1,54 @@
 // https://codepen.io/bramus/pen/eBZgPB
-// https://css-tricks.com/how-to-map-mouse-position-in-css/
+// https://css-tricks.com/how-to-map-position-in-css/
 // https://codersblock.com/blog/what-can-you-put-in-a-css-variable/
 // https://developer.mozilla.org/en-US/docs/Web/CSS/env()
 
-import wrapString from "../wrap-number-string@0.0.0/index.mjs";
 import CSSModel from "../css-model@0.0.0/index.mjs";
-
+const mousemove = function () {
+  const angle = Math.atan2(clientY, clientX) + Math.PI / 2;
+  const magnitude = Math.sqrt(clientX ** 2 + clientY ** 2);
+  const magnitudeNormalized = Math.sqrt(
+    (clientX / globalThis.innerWidth) ** 2 +
+      (clientY / globalThis.innerHeight) ** 2
+  );
+  this.set("x", clientX);
+  this.set("y", clientY);
+  this.setStr("x-str", clientX);
+  this.setStr("y-str", clientY);
+  this.set("ang", angle);
+  this.setStr("ang-str", angle);
+  this.set("mag", magnitude);
+  this.setStr("mag-str", magnitude);
+  this.set("magnorm", magnitudeNormalized);
+  this.setStr("magnorm-str", magnitudeNormalized);
+};
+const mouseup = function () {
+  this.set("up", "1");
+  this.set("down", "");
+};
+const mousedown = function () {
+  this.set("up", "");
+  this.set("down", "1");
+};
 const CSSModelMouse = class extends CSSModel {
-  constructor(target = globalThis.document) {
-    super(target);
-    this.set("mouse-up", "1");
-    target.addEventListener("mousemove", ({ clientX, clientY }) => {
-      const angle = Math.atan2(clientY, clientX) + Math.PI / 2;
-      const magnitude = Math.sqrt(clientX ** 2 + clientY ** 2);
-      const magnitudeNormalized = Math.sqrt(
-        (clientX / globalThis.innerWidth) ** 2 +
-          (clientY / globalThis.innerHeight) ** 2
-      );
-      this.set("mouse-x", clientX);
-      this.set("mouse-y", clientY);
-      this.set("mouse-x-str", wrapString(clientX));
-      this.set("mouse-y-str", wrapString(clientY));
-      this.set("mouse-ang", angle);
-      this.set("mouse-ang-str", wrapString(angle));
-      this.set("mouse-mag", magnitude);
-      this.set("mouse-mag-str", wrapString(magnitude));
-      this.set("mouse-magnorm", magnitudeNormalized);
-      this.set("mouse-magnorm-str", wrapString(magnitudeNormalized));
-    });
-
-    target.addEventListener("mouseup", () => {
-      this.set("mouse-up", "1");
-      this.set("mouse-down", "");
-    });
-
-    target.addEventListener("mousedown", () => {
-      this.set("mouse-up", "");
-      this.set("mouse-down", "1");
-    });
+  #mousemove;
+  #mouseup;
+  #mousedown;
+  constructor(target = globalThis.document, prefix = "mouse") {
+    super(target, prefix);
+    this.set("up", "1");
+    this.#mousemove = mousemove.bind(this);
+    this.#mouseup = mouseup.bind(this);
+    this.#mousedown = mousedown.bind(this);
+    this.target.addEventListener("mousemove", this.#mousemove);
+    this.target.addEventListener("mouseup", this.#mouseup);
+    this.target.addEventListener("mousedown", this.#mousedown);
+  }
+  detach() {
+    this.target.removeEventListener("mousemove", this.#mousemove);
+    this.target.removeEventListener("mouseup", this.#mouseup);
+    this.target.removeEventListener("mousedown", this.#mousedown);
+    return super.detach();
   }
 };
 
--- js/css-model@0.0.0/index.mjs
+++ js/css-model@0.0.0/index.mjs
@@ -1,24 +1,42 @@
+import wrapString from "../wrap-number-string@0.0.0/index.mjs";
+
 const CSSModel = class {
   #target;
+  #prefix;
   #tracked = new Set();
-  constructor(target = null) {
+  constructor(target = null, prefix = "") {
     if (!target) {
       throw new Error("No target provided");
     }
+    if (!prefix) {
+      throw new Error("No prefix provided");
+    }
     this.#target = target;
+    this.#prefix = prefix;
   }
   get target() {
     return this.#target;
   }
   set(name, value) {
-    this.#target.documentElement.style.setProperty(`--${name}`, value);
+    this.#target.documentElement.style.setProperty(
+      `--${this.#prefix}-${name}`,
+      value
+    );
     this.#tracked.add(name);
   }
+  setStr(name, value) {
+    this.set(name, wrapString(value));
+  }
   get(name) {
-    this.#target.documentElement.style.getPropertyValue(`--${name}`, value);
+    this.#target.documentElement.style.getPropertyValue(
+      `--${this.#prefix}-${name}`,
+      value
+    );
   }
   remove(name) {
-    this.#target.documentElement.style.removeProperty(`--${name}`);
+    this.#target.documentElement.style.removeProperty(
+      `--${this.#prefix}-${name}`
+    );
     this.#tracked.delete(name);
   }
   detach() {
