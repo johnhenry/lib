392ddc1065c8e4af6d4520d461a36effc64b1c22
(none) <john@iamjohnhenry.com>
2021-10-23 14:12:37 -0700

    add localstorage-cycler

---
+++ js/localstorage-cycler/0.0.0/index.mjs
@@ -0,0 +1,31 @@
+const createNext =
+  (key, handler, ...values) =>
+  (...events) => {
+    const stored = globalThis.localStorage.getItem(key);
+    const index = values.indexOf(stored) + 1;
+    const value = values[index] ?? values[0];
+    globalThis.localStorage.setItem(key, value);
+    return {
+      value,
+      key,
+      index,
+      result: handler({ value, key, index, events }),
+    };
+  };
+export default (key, ...values) => {
+  const handler = typeof values[0] === "function" ? values.shift() : () => {};
+  const stored = globalThis.localStorage.getItem(key) ?? values[0];
+  const index = values.indexOf(stored);
+  if (index === -1) {
+    throw new Error(`stored value not in list`);
+  }
+  const value = values[index];
+  globalThis.localStorage.setItem(key, value);
+  handler({
+    value,
+    key,
+    index,
+    events: [new CustomEvent("init", { detail: { value, key, index } })],
+  });
+  return createNext(key, handler, ...values);
+};
