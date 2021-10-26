3bc3ee7f928b29b11370ed48c4b34ffdd192e465
(none) <john@iamjohnhenry.com>
2021-10-26 02:19:44 -0700

    fix github link

--- scripts/buildindicies.sh
+++ scripts/buildindicies.sh
@@ -1,14 +1,16 @@
-# npm run template:get
-# npm run template:use --- default.html index.html
+npm run template:get
 ##
-npm run template:use --- demos/default.html demos/index.html
+npm run template:use -- default.html index.html
 ##
-npm run template:use --- demos/broadcast-channel/default.html demos/broadcast-channel/index.html
-npm run template:use --- demos/clock/default.html demos/clock/index.html
-npm run template:use --- demos/colorwheel/default.html demos/colorwheel/index.html
-npm run template:use --- demos/liedenticons/default.html demos/liedenticons/index.html
-npm run template:use --- demos/menu-component/default.html demos/menu-component/index.html
-npm run template:use --- demos/permutations/default.html demos/permutations/index.html
-npm run template:use --- demos/tester/default.html demos/tester/index.html
+npm run template:use -- bash/default.html bash/index.html
 ##
-npm run template:use --- bash/default.html bash/index.html
+npm run template:use -- demos/default.html demos/index.html
+##
+npm run template:use -- demos/broadcast-channel/default.html demos/broadcast-channel/index.html
+npm run template:use -- demos/clock/default.html demos/clock/index.html
+npm run template:use -- demos/colorwheel/default.html demos/colorwheel/index.html
+npm run template:use -- demos/liedenticons/default.html demos/liedenticons/index.html
+npm run template:use -- demos/menu-component/default.html demos/menu-component/index.html
+npm run template:use -- demos/permutations/default.html demos/permutations/index.html
+npm run template:use -- demos/tester/default.html demos/tester/index.html
+
