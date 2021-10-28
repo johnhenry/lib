01c503e2a05f7b2fa5e210f78582df35216ea377
(none) <john@iamjohnhenry.com>
2021-10-28 15:20:49 -0700

    co-locate demos

--- build.sh
+++ build.sh
@@ -2,10 +2,13 @@ sh script.sh update_template
 sh script.sh build_html js
 sh script.sh build_html css
 sh script.sh build_html html
+sh script.sh build_html bash
 sh script.sh build_indicies js
 sh script.sh build_indicies css
 sh script.sh build_indicies html
+sh script.sh build_indicies bash
 sh script.sh latest_versions js
 sh script.sh latest_versions css
 sh script.sh latest_versions html
+sh script.sh latest_versions bash
 sh script.sh run_create_test js
--- js/css-model-date/0.0.0/index.mjs
+++ js/css-model-date/0.0.0/index.mjs
@@ -1,6 +1,6 @@
 import CSSModel from "../../css-model/0.0.0/index.mjs";
 
-const update = function (UTC) {
+const update = function (model, UTC) {
   const date = new Date();
   const second = date[`get${UTC}Seconds`]();
   const minute = date[`get${UTC}Minutes`]();
@@ -11,40 +11,39 @@ const update = function (UTC) {
   const year = date[`get${UTC}FullYear`]();
   const am = hour24 < 12;
   const hour = hour24 === 0 || hour24 === 12 ? 12 : hour24 % 12;
-  this.set("second", second);
-  this.set("minute", minute);
-  this.set("hour", hour);
-  this.set("hour24", hour24);
-  this.set("weekday", weekday);
-  this.set("monthday", monthday);
-  this.set("month", month);
-  this.set("year", year);
-  this.set("am",am ? "1" : "");
-  this.set("pm",!am ? "1" : "");
-  this.set("ampm", am ? "am" : "pm");
+  model.set("second", second);
+  model.set("minute", minute);
+  model.set("hour", hour);
+  model.set("hour24", hour24);
+  model.set("weekday", weekday);
+  model.set("monthday", monthday);
+  model.set("month", month);
+  model.set("year", year);
+  model.set("am", am ? "1" : "");
+  model.set("pm", !am ? "1" : "");
+  model.set("ampm", am ? "am" : "pm");
 };
 
 const CSSModelDate = class extends CSSModel {
-  #update;
   #interval;
-  constructor(target = globalThis.document,  {utc=false, interval}={}, prefix = "date") {
-    super(target, prefix + utc ? "-utc" : "");
-    this.#update = update.bind(this, utc ? 'UTC' : '' );
-    this.update();
-    if(typeof interval === "number"){
-      this.#interval = setInterval(this.update, interval);
+  constructor(
+    target = globalThis.document,
+    { utc = false, interval } = {},
+    prefix = "date"
+  ) {
+    super(target, prefix + (utc ? "-utc" : ""));
+    const utcString = utc ? "UTC" : "";
+    update(this, utcString);
+    if (typeof interval === "number") {
+      this.#interval = setInterval(update, interval, this, utcString);
     }
   }
-  clear(){
+  clear() {
     clearInterval(this.#interval);
   }
-  detach() {
+  detach(...args) {
     this.clear();
-    delete this.#update;
-    return super.detach();
-  }
-  update() {
-    this.#update();
+    super.detach(...args);
   }
 };
 
