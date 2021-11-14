d570d5d38b624f4ee5c0588bcd56c672bfc7f418
johnhenry <john@iamjohnhenry.com>
2021-11-14 20:33:37 +0000

    Deploying to dist from @ johnhenry/lib@e8396760664c57c18a49f05b41706e37f5453d7f 🚀

--- js/css-model-audio-node/0.0.0/index.mjs
+++ js/css-model-audio-node/0.0.0/index.mjs
@@ -1,12 +1,30 @@
 import CSSModel from "../../css-model/0.0.0/index.mjs";
 const { AnalyserNode } = globalThis;
 
+const eightBitToColor = (num, ...order) => {
+  const colors = {};
+  if (!order.includes("red")) {
+    order.push("red");
+  }
+  if (!order.includes("green")) {
+    order.push("green");
+  }
+  if (!order.includes("blue")) {
+    order.push("blue");
+  }
+  colors[order.shift()] = (224 & num).toString(16).padStart(2, "0");
+  colors[order.shift()] = (28 & num).toString(16).padStart(2, "0");
+  colors[order.shift()] = (3 & num).toString(16).padStart(2, "0");
+  return `#${colors.red}${colors.green}${colors.blue}`;
+};
+
 export default class extends AnalyserNode {
   #running = false;
   #model;
   #timeDomainData;
   #frequencyData;
   #float;
+  #colorOrder;
   constructor(
     context,
     options,
@@ -16,7 +34,9 @@ export default class extends AnalyserNode {
   ) {
     super(context, options);
     this.#model = new CSSModel(target, prefix);
-    const { float = false, start = true } = moreOptions;
+    const { float = false, start = true, colorOrder } = moreOptions;
+
+    this.#colorOrder = colorOrder;
     this.#float = float;
     if (this.#float) {
       this.#timeDomainData = new Float32Array(this.fftSize);
@@ -53,10 +73,24 @@ export default class extends AnalyserNode {
       )
     );
     for (let x = 0; x < this.fftSize; x++) {
-      this.set(`td-${x}`, this.#timeDomainData[x]);
+      if (this.#colorOrder) {
+        this.set(
+          `td-color-${x}`,
+          eightBitToColor(this.#timeDomainData[x], ...this.#colorOrder)
+        );
+      } else {
+        this.set(`td-${x}`, this.#timeDomainData[x]);
+      }
     }
     for (let x = 0; x < this.frequencyBinCount; x++) {
-      this.set(`f-${x}`, this.#frequencyData[x]);
+      if (this.#colorOrder) {
+        this.set(
+          `f-color-${x}`,
+          eightBitToColor(this.#frequencyData[x], ...this.#colorOrder)
+        );
+      } else {
+        this.set(`f-${x}`, this.#frequencyData[x]);
+      }
     }
     globalThis.requestAnimationFrame(() => this.loop());
   }
