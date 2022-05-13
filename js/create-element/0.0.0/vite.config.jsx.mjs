7b96bebddbd51f98848b17c6dbcf55091c2cefeb
John Henry <john@iamjohnhenry.com>
2022-05-13 16:47:41 -0700

    Add vite.config.jsx.mjs

---
+++ js/create-element/0.0.0/vite.config.jsx.mjs
@@ -0,0 +1,3 @@
+export const jsxInject = `import _jsx from "https://johnhenry.github.io/lib/js/create-element/0.0.0/index.mjs";`;
+export const jsxFactory = "_jsx";
+export const jsxFragment = "''";
